2dec120bdcf7d8efe83e7f21ba0666ef667dc410
hechenglong <kfhechenglong@126.com>
2018-01-02 18:15:40 +0800

    aaa

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
 const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
 	// 入口文件
-	entry : 'js/index.js',
+	entry : './js/index.js',
 	output:{
-		path:path.resolve(__dirname,'dlist'),
-		filename:''
-	}
+		path:path.resolve(__dirname,'dist'),
+		filename:'index_webpack.js'
+	},
+	// 配置本地服务器
+	devServer:{
+		port:9090,
+		hot: true,
+		inline:true
+	},
+	plugins:[
+		new HtmlWebpackPlugin({
+			title:'ahahh',
+			filename:'dist/index.html'
+			template:'index.html'
+		})
+	]
 }
