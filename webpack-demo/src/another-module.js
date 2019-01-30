900c2ec92a385f4f74897df12c45a20db46d82a1
hechenglong <kfhechenglong@126.com>
2019-01-30 13:32:37 +0800

    mock

--- webpack-demo/src/another-module.js
+++
@@ -1,2 +0,0 @@
-import _ from 'lodash';
-console.log(_.jion(['Another','module','loded!'],''))
--- webpack-demo/src/print.js
+++
@@ -1,5 +0,0 @@
-
-console.log('adasd')
-export default function print() {
-    console.log('this is print js');
-}
